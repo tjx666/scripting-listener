7e6cab85e80651042664a4271038f6c3b291af6e
长风 <changfeng@gaoding.com>
2022-03-16 01:05:49 +0800

    build: optimize scripts and debug launch config

--- test/sample.test.ts
+++ test/sample.test.ts
@@ -1,21 +1,9 @@
 import { strictEqual } from 'assert';
 import * as vscode from 'vscode';
 
-import helloWorld from '../src/helloWorld';
-
 describe('#test sample', () => {
     before(() => {
         vscode.window.showInformationMessage('Test begin!');
-    });
-
-    it('hello world', async () => {
-        helloWorld();
-        // delay to see the vscode notification
-        return new Promise((resolve) => {
-            setTimeout(() => {
-                resolve();
-            }, 1.5 * 1000);
-        });
     });
 
     it('one plus one equals two', () => {
