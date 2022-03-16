55e3015db2938ebc9ae4c4b52a1b848ce4a13d1f
长风 <changfeng@gaoding.com>
2022-03-17 03:15:29 +0800

    perf: remove useless condition and string slice

--- src/LogViewer/readLastLines.ts
+++ src/LogViewer/readLastLines.ts
@@ -36,7 +36,11 @@ export default async function readLastLines(
         throw new Error('file does not exist');
     }
 
-    const NEW_LINE_CHARACTERS = new Set(['\n']);
+    if (maxLineCount < 0) {
+        throw new Error('maxLineCount should >= 1, but you pass: ' + maxLineCount);
+    }
+
+    const lineSeparator = '\n';
     const [fileStat, fileHandle] = await Promise.all([
         fsAsync.stat(filePath),
         fsAsync.open(filePath, 'r'),
@@ -45,13 +49,10 @@ export default async function readLastLines(
     let lineCount = 0;
     let linesStr = '';
 
-    async function readRecursive(): Promise<string> {
-        if (linesStr.length > fileStat.size) {
-            linesStr = linesStr.slice(Math.max(0, linesStr.length - fileStat.size));
-        }
-
-        if (linesStr.length >= fileStat.size || lineCount >= maxLineCount) {
-            if (NEW_LINE_CHARACTERS.has(linesStr.slice(0, 1))) {
+    // eslint-disable-next-line no-constant-condition
+    while (true) {
+        if (linesStr.length === fileStat.size || lineCount === maxLineCount) {
+            if (linesStr[0] === lineSeparator) {
                 linesStr = linesStr.slice(1);
             }
             fileHandle.close();
@@ -64,25 +65,22 @@ export default async function readLastLines(
             currentCharacterCount,
         );
         linesStr = nextCharacter + linesStr;
-        if (NEW_LINE_CHARACTERS.has(nextCharacter) && linesStr.length > 1) {
+        if (nextCharacter === lineSeparator) {
             lineCount++;
         }
         currentCharacterCount++;
-        return readRecursive();
     }
-
-    return readRecursive();
 }
 
 async function main() {
     console.time('readLastLines');
     const testPath = '/Users/yutengjing/code/scripting-listener/package-lock.json';
-    const lines = await readLastLines(testPath, 100, 'utf8');
+    const lines = await readLastLines(testPath, 4000, 'utf8');
     // console.log(`[${lines}]`);
     console.timeEnd('readLastLines');
 
-    console.time('readFile');
-    const content = await fsAsync.readFile(testPath, { encoding: 'utf8' });
-    console.timeEnd('readFile');
+    // console.time('readFile');
+    // const content = await fsAsync.readFile(testPath, { encoding: 'utf8' });
+    // console.timeEnd('readFile');
 }
 main();
